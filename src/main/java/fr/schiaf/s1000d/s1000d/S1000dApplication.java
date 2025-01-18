8604c1fc6ca0c5079077178b35dbb94d87d915ad
schiaf <schiaf@hotmail.com>
2025-01-19 00:59:32 +0100

    init project

---
+++ src/main/java/fr/schiaf/s1000d/s1000d/S1000dApplication.java
@@ -0,0 +1,13 @@
+package fr.schiaf.s1000d.s1000d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S1000dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S1000dApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/schiaf/s1000d/s1000d/S1000dApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.schiaf.s1000d.s1000d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S1000dApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
