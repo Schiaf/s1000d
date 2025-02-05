227297f7cb95db3981e75903d8bef010334de64b
schiaf <schiaf@hotmail.com>
2025-02-05 01:32:23 +0100

    Fix merge issues

--- src/main/java/fr/schiaf/s1000d/service/ElementService.java
+++ src/main/java/fr/schiaf/s1000d/service/ElementService.java
@@ -3,7 +3,7 @@ package fr.schiaf.s1000d.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import fr.schiaf.s1000d.model.dmodule.Element;
+import fr.schiaf.s1000d.model.dmodule.ElementXML;
 import fr.schiaf.s1000d.model.dmodule.ElementFactory;
 
 @Service
@@ -13,9 +13,9 @@ public class ElementService {
     private ElementFactory elementFactory;
 
     public void processElements() {
-        Element dmodule = elementFactory.createElement("dmodule");
-        Element identAndStatusSection = elementFactory.createElement("identAndStatusSection");
-        Element dmAddress = elementFactory.createElement("dmAddress");
+        ElementXML dmodule = elementFactory.createElement("dmodule");
+        ElementXML identAndStatusSection = elementFactory.createElement("identAndStatusSection");
+        ElementXML dmAddress = elementFactory.createElement("dmAddress");
         identAndStatusSection.getChildren().add(dmAddress);
         dmodule.getChildren().add(identAndStatusSection);
         System.out.println(dmodule.toS1000DXml());
